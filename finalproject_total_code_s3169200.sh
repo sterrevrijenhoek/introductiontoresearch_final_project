fe6e64ae6a777b90ac0222e7c15da720328664f7
Sterre Vrijenhoek <60843690+sterrevrijenhoek@users.noreply.github.com>
2020-03-28 10:51:20 +0100

    Create finalproject_total_code_s3169200.sh

---
+++ finalproject_total_code_s3169200.sh
@@ -0,0 +1,12 @@
+# !/usr/bin
+# File name: finalproject_total_code_s3169200.sh
+# Author: S. Vrijenhoek
+# Date: 28/03/2020
+
+
+function TOTAL_TWEETS {
+	"""counts the amount of total tweets in a Karora Twitter data directory"""
+	wc -l
+}
+
+TOTAL_TWEETS
